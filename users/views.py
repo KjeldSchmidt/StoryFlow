4cb18b50dec0208732184489eef427149895b497
Kjeld Schmidt <contact@kjeld-schmidt.com>
2018-10-03 16:35:50 +0200

    Login and Logout now stay at the last page instead of redirecting to index

--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import HttpResponseRedirect
+
+
+def redirect_to_origin( request ):
+    return HttpResponseRedirect( request.META.get( 'HTTP_REFERER', '/' ) )
 
 
 def login_view( request ):
@@ -8,15 +12,15 @@ def login_view( request ):
     user = authenticate( request, username=username, password=password )
     if user is not None:
         login( request, user )
-        return redirect( 'index' )
+        return redirect_to_origin( request )
     else:
-        return redirect( 'index' )
+        return redirect_to_origin( request )
 
 
 def login_failed( request ):
-    return redirect( 'index' )
+    return redirect_to_origin( request )
 
 
 def logout_view( request ):
     logout( request )
-    return redirect( 'index' )
+    return redirect_to_origin( request )
