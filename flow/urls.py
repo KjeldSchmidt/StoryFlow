efde3c4575e546c7e11b3c1d7d08e6ed13916b00
Kjeld Schmidt <contact@kjeld-schmidt.com>
2018-10-11 13:52:47 +0200

    Made all links to name rather than path.

--- flow/urls.py
+++ flow/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path( '', views.index, name='index' ),
-    path( 'signup/', views.signup, name="signup" ),
+    path( 'signup', views.signup, name="signup" ),
     path( 'game/create', views.create_game, name="create_game" ),
     path( 'game/<int:game_id>', views.game_by_id, name="game" ),
     path( 'game/<int:game_id>/edit', views.edit_game, name="edit_game" ),
@@ -13,6 +13,6 @@ urlpatterns = [
     path( 'games/list/all', views.games_list_all, name='games_list_all' ),
     path( 'games/list/mine', views.games_list_mine, name='games_list_mine' ),
     path( 'games/list/played', views.games_list_played, name='games_list_played' ),
-    path( 'games/<int:game_id>/edit/chat/send/', views.receive_edit_chat, name='send_edit_chat' ),
-    path( 'games/<int:game_id>/edit/chat/get/', views.get_edit_chat, name='get_edit_chat' ),
+    path( 'games/<int:game_id>/edit/chat/send', views.receive_edit_chat, name='send_edit_chat' ),
+    path( 'games/<int:game_id>/edit/chat/get', views.get_edit_chat, name='get_edit_chat' ),
 ]
